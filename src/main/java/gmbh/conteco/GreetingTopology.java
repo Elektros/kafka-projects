644d72dae2e3f6f738670b4cd76e54b41eaf0efb
Elektros <frieseneugen92@gmail.com>
2022-11-09 16:37:16 +0100

    Fixes

--- src/main/java/gmbh/conteco/GreetingTopology.java
+++ src/main/java/gmbh/conteco/GreetingTopology.java
@@ -10,10 +10,10 @@ public class GreetingTopology {
     public static Topology build() {
         StreamsBuilder builder = new StreamsBuilder();
 
-        builder.stream("persons", Consumed.with(Serdes.Void(), Serdes.String()))
+        builder.stream("persons", Consumed.with(Serdes.String(), Serdes.String()))
                 .filterNot((key, value) -> value.equals("Eugen"))
                 .mapValues(value -> "Hallo " + value)
-                .to("greetings", Produced.with(Serdes.Void(), Serdes.String()));
+                .to("greetings", Produced.with(Serdes.String(), Serdes.String()));
 
         return builder.build();
     }
