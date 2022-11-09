fd03329988735ab3898f4a0bad14294e53c9980c
Elektros <frieseneugen92@gmail.com>
2022-11-09 11:01:23 +0100

    load properties

--- src/main/java/gmbh/conteco/TransactionalProducer.java
+++ src/main/java/gmbh/conteco/TransactionalProducer.java
@@ -2,6 +2,10 @@ package gmbh.conteco;
 
 public class TransactionalProducer {
     public static void main(String[] args) {
+        Properties properties = new Properties();
 
+        try(InputStream stream = TransactionalProducer.class.getResourceAsStream("/application.properties")) {
+            properties.load(stream);
+        }
     }
 }
